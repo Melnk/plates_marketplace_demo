f177031fe375e76b8e30bcce8d0cb27797ad0bf5
Melnik <melnikoleg080@gmail.com>
2025-11-21 18:01:37 +0300

    created KafkaProducer

---
+++ src/main/java/com/plates/service/KafkaProducer.java
@@ -0,0 +1,27 @@
+package com.plates.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.plates.entity.Plate;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendPlateCreated(Plate plate) {
+        try {
+            String json = mapper.writeValueAsString(plate);
+            kafkaTemplate.send("plate.created", json);
+            System.out.println("Sent to Kafka" + json);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to send plate to Kafka: " + plate.getNumber(), e);
+        }
+    }
+}
